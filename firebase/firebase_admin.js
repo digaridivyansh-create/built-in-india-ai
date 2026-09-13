dae5c1f3044f166843a9ba5f9221120bf8cb0411
digaridivyansh-create <digaridivyansh@gmail.com>
2026-09-13 16:00:03 +0530

    Use environment variables for Firebase deployment

--- firebase/firebase_admin.js
+++ firebase/firebase_admin.js
@@ -1,10 +1,11 @@
-const { initializeApp, cert } = require("firebase-admin/app");
+﻿const { initializeApp, cert } = require("firebase-admin/app");
 const { getDatabase } = require("firebase-admin/database");
-const path = require("path");
 
-const serviceAccount = require(
-    path.join(__dirname, "..", "config", "serviceAccountKey.json")
-);
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+};
 
 const app = initializeApp({
     credential: cert(serviceAccount),
